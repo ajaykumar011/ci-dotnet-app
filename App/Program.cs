b5913d5ad353d0302ae8573a2bbbc7d5527dcf0b
Ajay Kumar <ajay011.sharma@hotmail.com>
2023-01-27 09:49:37 +0000

    try to push to ecr

--- App/Program.cs
+++ App/Program.cs
@@ -1,32 +1,63 @@
 using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 using Amazon.S3;
-using Amazon.S3.Model;
-using Amazon.S3.Util;
 using System;
 using System.Threading.Tasks;
-     
-namespace App
+//https://github.com/aws/aws-sdk-net/issues/2477
+namespace S3
 {
-  class Program
-  {
-   async static Task Main(string[] args)
-   {
-         
-      var bucketRegion = RegionEndpoint.APSouth1;
-      // Note: You could add more valid AWS Regions above as needed.
+    // docker run --rm -it -v "$(Get-Location):/src" -v "$env:USERPROFILE/.aws:/root/.aws:ro" -w /src mcr.microsoft.com/dotnet/core/sdk:3.1 dotnet run
+    // Found 327 buckets.
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // AWSCredentials credentials;
+
+            // if (Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID") != null)
+            // {
+            //     Console.WriteLine("Getting credentials from environment variables.");
+            //     credentials = new EnvironmentVariablesAWSCredentials();
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Getting credentials from credentials files.");
+
+            //     var profileName = args.Length == 0 ? "default" : args[0];
+
+            //     var sharedFile = new SharedCredentialsFile();
+
+            //     if (!sharedFile.TryGetProfile(profileName, out var profile))
+            //     {
+            //         throw new ArgumentException($"AWS profile '{profileName}' was not found.");
+            //     }
+
+            //     if (!AWSCredentialsFactory.TryGetAWSCredentials(profile, sharedFile, out credentials))
+            //     {
+            //         throw new NotSupportedException($"Failed to get AWS credentials from profile named '{profileName}'.");
+            //     }
+            // }
+
+            // var s3Client = new AmazonS3Client(credentials, RegionEndpoint.USEast1);
+
+            var bucketRegion = RegionEndpoint.APSouth1;
+            // Note: You could add more valid AWS Regions above as needed.
      
-      using (var s3Client = new AmazonS3Client(bucketRegion)) {
-      var bucketName = args[0];       
-             
-        // List current buckets.
-       Console.WriteLine("\nMy buckets now are:");
-       var response = await s3Client.ListBucketsAsync();
-        
-       foreach (var bucket in response.Buckets)
-       {
-       Console.WriteLine(bucket.BucketName);
-       }
-      }
+            using (var s3Client = new AmazonS3Client(bucketRegion)) {
+                var response = await s3Client.ListBucketsAsync();
+                Console.WriteLine($"Found {response.Buckets.Count} buckets.");
+                await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+                Console.WriteLine($"Execution Completed");
+                // List current buckets.
+                Console.WriteLine("\nMy buckets now are:");
+                var s3response = await s3Client.ListBucketsAsync();
+                    
+                foreach (var bucket in s3response.Buckets)
+                {
+                    Console.WriteLine(bucket.BucketName);
+                }
+            }
+        }
     }
-  }
 }
